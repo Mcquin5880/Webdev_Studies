c773f916406cfd21455da07ba942ee6cdf9468f8
NewbieMcCoder <Mquinter1988@gmail.com>
2020-11-13 20:01:03 -0500

    Section wrap-up

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,41 +1,54 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+var workItems = [];
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res) {
 
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      console.log("Whoa, what!?");
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.post("/", function(req, res) {
+
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/Work");
+  } else {
+    items.push(item);
+    res.redirect("/");
   }
-  res.render("list", {kindOfDay: day});
+
+});
+
+app.post("/work", function(req, res) {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, function() {
